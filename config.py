3bd0a585cc064e5d38e0598d0109440c4c228735
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2015-10-18 21:54:20 -0200

    Añado archivo de configuración
    Archivo de configuración

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Check_data especifica si debe chequearse o no la informacion del recordset. 
+check_data = True
+#Corrector = Indica el valor de correcion a aplicar en x.
+corrector = -2
+#Alto maximo que puede tener una tupla en pantalla.
+max_y_cuadro_threshold = 20
+#Threshold de analisis al reconstruir datos de screen.lista_tipo_datos.
+analyze_threshold = 10
+#Con esta opcion los errores se muestran en pantalla luego de la impresion.
+show_errors = True
+# Registro de errores en el log del programa.
+log_errors = True
