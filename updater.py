22f9a3b0532cf691a37daa21e7b5ddb8a785f000
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2015-10-19 22:13:11 -0200

    Añado actualizador

---
+++ updater.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+
+#Procede a actualizar Outconty en caso de necesidad.
+def update():
+	if check_update:
+		try:
+			os.system('wget https://github.com/carlosplanchon/outfancy/archive/master.zip')
+		except:
+			return 'Error al chequear actualizacion, compruebe su conexion a internet. '
+		os.system('unzip master.zip && rm -rf outfancy && mv outfancy-master outfancy && rm master.zip')
+	return '--- Actualizado ---'
+
+#Chequea la version, en caso de necesitar actualizacion devuelve True.
+def check_update():
+	os.renames('version.py', 'check_version.py')
+	try:
+		version_py = os.system('wget https://raw.githubusercontent.com/carlosplanchon/outfancy/master/version.py')
+	except:
+		print('Error al chequear actualizacion, revise su conexion a internet')
+		return False
+	import check_version, version
+	if check_version.__version__ == version.__version__:
+		need_update = False
+	else:
+		need_update = True
+	os.remove('version.py')
+	os.renames('check_version.py', 'version.py')
+
+	return need_update
