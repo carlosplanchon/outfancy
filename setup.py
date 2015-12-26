162ac00d8f2c15b971d6adf3e1ee935a60693cac
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2015-12-26 19:21:23 -0300

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name = 'outfancy',
     packages = ['outfancy'],
-    version = '0.2.0',
+    version = '0.2.3',
     description = 'Sistema de renderizado para salidas en pantalla (tty). / Rendering system for screen outputs.',
     author = 'Carlos A. Planchon',
     author_email = 'urucrafters@gmail.com',
     url = 'https://github.com/carlosplanchon/outfancy',
-    download_url = 'https://github.com/carlosplanchon/outfancy/archive/0.2.0.tar.gz',
+    download_url = 'https://github.com/carlosplanchon/outfancy/archive/0.2.3.tar.gz',
     keywords = ['rendering', 'graphical', 'terminal'],
     classifiers = [],
 )
