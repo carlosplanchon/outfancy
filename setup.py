e25ef9f883de2c18cbd378c5868198f5ae2daefd
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2016-05-06 15:50:49 -0300

    0.5.1 Version :D

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name = 'outfancy',
     packages = ['outfancy'],
-    version = '0.5.0',
+    version = '0.5.1',
     description = 'Python3 library to print tables in Terminal / Libreria en Python3 para imprimir planillas en Terminal.',
     author = 'Carlos A. Planchon',
     author_email = 'urucrafters@gmail.com',
