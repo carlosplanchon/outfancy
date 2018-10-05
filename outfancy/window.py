8e7391bd348a88098c39d407eb1104a3e0a4f343
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2018-10-05 00:49:11 -0300

    Fixing a bug.

--- outfancy/window.py
+++ outfancy/window.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python3
 
-import utils
+from . import widgets
 
 
 class Window:
     """It creates a Window Object."""
-    def __init__(self, width, heigth, fill=' '):
-        self.content = utils.create_matrix(width, heigth, fill)
+    def __init__(self, width, height, fill=' '):
+        self.content = widgets.create_matrix(
+            x=width,
+            y=height,
+            fill=fill
+            )
 
     def insert(self, matrix, x_vertex, y_vertex):
+        """Each element of the matrix is inserted on the window."""
+        # Variable to walk through the matrix (y value).
+        y_index = 0
+        while y_index < len(matrix) and y_vertex < len(self.content):
+            # Variable to walk through the matrix (x value).
+            x_index = 0
+            x_vert = x_vertex
+            while x_index < len(matrix[0]) and x_vert < len(self.content[0]):
+                self.content[y_vertex][x_vert] = matrix[y_index][x_index]
+                x_index += 1
+                x_vert += 1
+            y_index += 1
+            y_vertex += 1
+
+    def insert_point(self, point_character, x_coord, y_coord):
         """Each element of the matrix is inserted in the window."""
-        # We want to work in a matrix and a screen with elements.
-        if len(matrix) > 0 and len(self.content) > 0:
-            # Variable to control where we want to insert each point (y value).
-            y_to_insert = y_vertex
-            # Variable to walk through the matrix (y value).
-            y_index = 0
-            while y_to_insert < len(self.content):
-                # Variable to control where we want to insert each point
-                # (x value).
-                x_to_insert = x_vertex
-                # Variable to walk through the matrix (x value).
-                x_index = 0
-                while x_to_insert < len(self.content[0]):
-
-                    self.content[
-                        y_to_insert][x_to_insert] = matrix[y_index][x_index]
-
-                    x_to_insert += 1
-                    x_index += 1
-
-                y_to_insert += 1
-                y_index += 1
-
-    def insert_point(self, point, x_coord, y_coord):
-        """Each element of the matrix is inserted in the window."""
-        self.content[y_coord][x_coord] = point
+        self.content[y_coord][x_coord] = point_character
 
     def render(self):
         return '\n'.join([''.join(x) for x in self.content])
