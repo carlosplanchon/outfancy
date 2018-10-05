8780609e48d11d6ed1937c61a0b3d03508afae6e
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2018-10-05 00:49:55 -0300

    Refactoring.

--- outfancy/utils.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-
-def create_matrix(x, y, fill=''):
-    """This function create a 2d matrix based on it dimensions."""
-    return [[fill for p in range(x)] for p in range(y)]
